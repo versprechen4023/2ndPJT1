7c2f228a8ebf7f8161989af51e3af3a9058bc171
ITWILL <ITWILL@DESKTOP-7JIVS4K>
2023-10-04 17:47:16 +0900

    231004 commit / 업데이트 에이잭스,검색,셀렉트태그 값 받아오기.

--- icemile/src/main/java/ems/icemile/controller/BuyAjaxController.java
+++ icemile/src/main/java/ems/icemile/controller/BuyAjaxController.java
@@ -2,6 +2,8 @@ package ems.icemile.controller;
 
 import javax.inject.Inject;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,11 +21,16 @@ public class BuyAjaxController {
 	@Inject // 바이 서비스 의존성 주입
 	private BuyServiceImpl buyService;
 	
-	@PostMapping("/insert")
-	public void buyInsert(BuyDTO buyDTO) {
-		
-		 buyService.buyInsert(buyDTO);
-	}
+    @PostMapping("/insert")
+    public ResponseEntity<String> buyInsert(BuyDTO buyDTO) {
+        try {
+            buyService.buyInsert(buyDTO);
+            return new ResponseEntity<>("true", HttpStatus.OK); // 성공 시 "true" 반환
+        } catch (Exception e) {
+            log.error("Error during buy insertion: {}", e.getMessage());
+            return new ResponseEntity<>("false", HttpStatus.INTERNAL_SERVER_ERROR); // 실패 시 "false" 반환
+        }
+    }
 	
 	/*
 	 * @PostMapping("/update") public String buyUpdate(BuyDTO buyDTO) {
